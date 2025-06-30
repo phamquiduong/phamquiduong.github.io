86122b2e9a382b2e8a2916df93366ebb5b225c1f
phamquiduong <phamquiduong@outlook.com>
2025-07-01 03:46:56 +0700

    Update detail and fix some animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-// Auto switch theme
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
-const setTheme = () => {
-  const theme = prefersDark.matches ? 'dark' : 'light'
-  document.documentElement.setAttribute('data-bs-theme', theme)
-}
-setTheme()
-prefersDark.addEventListener('change', setTheme)
-
 // Set space
 const avatar = document.querySelector('.avatar')
 const profileSpace = document.querySelector('.profile-space')
